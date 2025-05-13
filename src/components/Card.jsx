0fd4707fbfef51e4cf2652385d796864825d1c92
iliast <ilias.thdrds@gmail.com>
2025-05-13 05:06:16 +0300

    Style & layout fixes: aligned location icon, adjusted image size, flex row for card info

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,14 +3,15 @@ import React from "react";
 export default function Card(props) {
 
 return (
-<div className="Card">
+<div className="card">
 <img src={`./${props.Img}`} className="card--image" alt="card image" />
  
 <div className="card--info">
-   <img src="./location.png" className="card--loc" alt="star" />
+   <img src="./location.png" className="card--loc" alt="star" /> 
+   <span className="card--loocation" >{props.Location}</span>
+  <a className="card--maps" href={props.GoogleMaps} target="_blank" rel="noopener noreferrer">View on Google Maps</a>
 
-</div>
-
+<p className="card--title">{props.Title}</p></div>
 </div>
 )
 }
--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,9 +1,9 @@
 const dataarey=[
     {
         id:"1",
-        Location:"South Korea",
-        GoogleMaps:"",
-        Title:"Busan",
+        Location:"KOREA",
+        GoogleMaps:"https://maps.app.goo.gl/p25FTDpDokDF9nU89",
+        Title:"Busan Dong-a University",
         Date:"2007",
         Description:" Busan, South Korea’s second-largest city, is renowned for its stunning beaches, vibrant culture, and fresh seafood. The city is surrounded by beautiful mountains and offers an eclectic mix of modern attractions like the Busan Tower, historic temples, and bustling markets. It’s a dynamic coastal destination for travelers.",
         Img:"busan.jpg"
@@ -12,16 +12,16 @@ const dataarey=[
         id:"2",
         Location:"UAE",
         GoogleMaps:"",
-        Title:"Dubai",
+        Title:"Dubai Palm Tree Beach",
         Date:"2005",
         Description:"Dubai is a futuristic city known for its luxurious lifestyle, cutting-edge architecture, and towering skyscrapers. The city offers a blend of modernity and tradition, with attractions like the Burj Khalifa, luxury shopping malls, and vast deserts for adventure. It’s a global hub for business and tourism.",
         Img:"dubai.jpg"
     },
     {
         id:"3",
-        Location:"Thailand",
+        Location:"THAILAND",
         GoogleMaps:"",
-        Title:"Bangong",
+        Title:"Bangong night life",
         Date:"2008",
         Description:"Bangkok, Thailand’s capital, is a lively metropolis famous for its bustling street markets, ornate temples, and vibrant nightlife. Visitors can explore cultural landmarks like the Grand Palace and Wat Pho, enjoy delicious street food, and experience the vibrant energy of one of Asia’s most exciting cities.",
         Img:"bangong.jpg"
